b6f7cfe18c942047a01adcce43855283c58f95f3
Divya Savai <divyasam1027@gmail.com>
2023-06-15 19:48:05 +0200

    city index page & event show page

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,6 +2,7 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
+
 import { application } from "./application"
 
 import AddressAutocompleteController from "./address_autocomplete_controller"
@@ -15,3 +16,6 @@ application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
+
+import EventsController from "./events_index_controller"
+application.register("events-index", EventsController)
